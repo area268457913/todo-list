1039b480d4f018f682740edbc25dcf218f1c43bc
area268457913 <area268457913@hotmail.com>
2021-05-02 15:47:54 +0800

    feat: add detail page

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express')
-const mongoose = require('mongoose')// 載入 mongoose
+// 載入 mongoose
+const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
-const Todo = require('./models/todo')//載入todo model
-const bodyParser = require('body-parser')// 引用 body-parser
+//載入todo model
+const Todo = require('./models/todo')
+// 引用 body-parser
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })// 設定連線到 mongoDB
+// 設定連線到 mongoDB
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
 
 // 取得資料庫連線狀態
 const db = mongoose.connection
@@ -19,6 +23,7 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
@@ -40,6 +45,14 @@ app.post('/todos', (req, res) => {
   const name = req.body.name // 從 req.body拿出表單裡的 name 資料
   return Todo.create({ name })//存入資料庫
     .then(() => res.redirect('/'))// 新增完成後導回首頁
+    .catch(error => console.log(error))
+})
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)// findById 以ＩＤ 去尋找
+    .lean()
+    .then((todo) => res.render('detail', { todo }))
     .catch(error => console.log(error))
 })
 
