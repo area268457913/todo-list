162d9b2da66331c23d169f47cc95aae6e4c72770
area268457913 <area268457913@hotmail.com>
2021-05-09 18:38:51 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 //載入todo model
-const Todo = require('./models/todo')
+// const Todo = require('./models/todo')
 // 引用 body-parser
 const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 // 載入 method-override
 const methodOverride = require('method-override')
+// 引用路由器
+const routes = require('./routes')
 
 // 設定連線到 mongoDB
 mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -31,66 +33,69 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // 設定每一筆請求都會透過 methodOverride 進行前置處理
 app.use(methodOverride('_method'))
 
+// 將 request 導入路由器
+app.use(routes)
+
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
-app.get('/', (req, res) => {
-  Todo.find()//取todo model 裡的所有資料
-    .lean()//把 mongoose 的 model 物件轉成乾淨的javascript 資料陣列
-    .sort({ _id: 'asc' })// 新增這裡：根據 _id 升冪排序
-    //sort() 是 Mongoose 提供的排序方法，裡面的參數 { _id: 'asc' } 代表「根據 _id 用升冪 (ascending) 排序」，反之，如果要降冪 (desscending) 排序，可以寫 'desc'。
-    .then(todos => res.render('index', { todos }))//將資料傳給index 樣板
-    .catch(error => console.error(error))//錯誤處理
+// app.get('/', (req, res) => {
+//   Todo.find()//取todo model 裡的所有資料
+//     .lean()//把 mongoose 的 model 物件轉成乾淨的javascript 資料陣列
+//     .sort({ _id: 'asc' })// 新增這裡：根據 _id 升冪排序
+//     //sort() 是 Mongoose 提供的排序方法，裡面的參數 { _id: 'asc' } 代表「根據 _id 用升冪 (ascending) 排序」，反之，如果要降冪 (desscending) 排序，可以寫 'desc'。
+//     .then(todos => res.render('index', { todos }))//將資料傳給index 樣板
+//     .catch(error => console.error(error))//錯誤處理
 
-})
+// })
 
-app.get('/todos/new', (req, res) => {
-  return res.render('new')
-})
+// app.get('/todos/new', (req, res) => {
+//   return res.render('new')
+// })
 
-app.post('/todos', (req, res) => {
-  const name = req.body.name // 從 req.body拿出表單裡的 name 資料
-  return Todo.create({ name })//存入資料庫
-    .then(() => res.redirect('/'))// 新增完成後導回首頁
-    .catch(error => console.log(error))
-})
+// app.post('/todos', (req, res) => {
+//   const name = req.body.name // 從 req.body拿出表單裡的 name 資料
+//   return Todo.create({ name })//存入資料庫
+//     .then(() => res.redirect('/'))// 新增完成後導回首頁
+//     .catch(error => console.log(error))
+// })
 
-app.get('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)// findById 以ＩＤ 去尋找
-    .lean()
-    .then((todo) => res.render('detail', { todo }))
-    .catch(error => console.log(error))
-})
+// app.get('/todos/:id', (req, res) => {
+//   const id = req.params.id
+//   return Todo.findById(id)// findById 以ＩＤ 去尋找
+//     .lean()
+//     .then((todo) => res.render('detail', { todo }))
+//     .catch(error => console.log(error))
+// })
 
-app.get('/todos/:id/edit', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .lean()
-    .then((todo) => res.render('edit', { todo }))
-    .catch(error => console.log(error))
-})
+// app.get('/todos/:id/edit', (req, res) => {
+//   const id = req.params.id
+//   return Todo.findById(id)
+//     .lean()
+//     .then((todo) => res.render('edit', { todo }))
+//     .catch(error => console.log(error))
+// })
 
-app.put('/todos/:id/', (req, res) => {
-  const id = req.params.id
-  const { name, isDone } = req.body
-  return Todo.findById(id)
-    .then(todo => {
-      todo.name = name
-      todo.isDone = isDone === 'on'
-      return todo.save()
-    })
-    .then(() => res.redirect(`/todos/${id}`))
-    .catch(error => console.log(error))
-})
+// app.put('/todos/:id/', (req, res) => {
+//   const id = req.params.id
+//   const { name, isDone } = req.body
+//   return Todo.findById(id)
+//     .then(todo => {
+//       todo.name = name
+//       todo.isDone = isDone === 'on'
+//       return todo.save()
+//     })
+//     .then(() => res.redirect(`/todos/${id}`))
+//     .catch(error => console.log(error))
+// })
 
-app.delete('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .then(todo => todo.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
+// app.delete('/todos/:id', (req, res) => {
+//   const id = req.params.id
+//   return Todo.findById(id)
+//     .then(todo => todo.remove())
+//     .then(() => res.redirect('/'))
+//     .catch(error => console.log(error))
+// })
 
 app.listen(port, () => {
   console.log(`App is running on http://localhost:${port}`)
