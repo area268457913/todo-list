4635057a8bb6b6f5e86a0b81c3b2a54fa6d43575
area268457913 <area268457913@hotmail.com>
2021-05-27 17:36:30 +0800

    feat: add user model

---
+++ models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')// 載入 mongoose
+const Schema = mongoose.Schema
+const userSchema = new Schema({
+  name: {
+    type: String, // 資料型別是字串
+    require: true // 這是必填欄位，不能空白
+  },
+  email: {
+    type: String,
+    require: true
+  },
+  password: {
+    type: String,
+    require: true
+  },
+  createAt: { // 使用者註冊時間
+    type: Date,
+    default: Date.now // default設定預設值， date.now使用者的當下時間
+  }
+
+
+})
+module.exports = mongoose.model('User', userSchema)// 透過 module.exports 把這個 schema 輸出，匯出的時候我們把這份 schema 命名為 Todo，以後在其他的檔案直接使用 Todo 就可以操作和「待辦事項」有關的資料
